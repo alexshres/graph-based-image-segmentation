89a5b6bfe553e4e7a37ec8163eca2a51b7ecd87d
Alex Shrestha <alexshrestha1996@gmail.com>
2025-03-11 21:48:22 -0700

    starting git repo

---
+++ main.py
@@ -0,0 +1,25 @@
+# Alex Shrestha
+# Efficient Image Segmentation Implementation
+# FILE: main.py 
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import unionfind as uf
+import utils
+
+
+def main():
+    print("hello")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
+
+
