506356a4a94397b27b5e559e1322ab06e75341ad
Alex Shrestha <alexshrestha1996@gmail.com>
2025-03-12 13:05:31 -0700

    added images

--- segmentation.py
+++ segmentation.py
@@ -1,8 +1,7 @@
 import numpy as np
 import unionfind as uf
 
-
-
+from utils import flattened_to_coordinates, get_neighbors
 
 class SegmentImage:
     def __init__(self, image):
@@ -10,6 +9,13 @@ class SegmentImage:
         self.size = image.shape[0] * image.shape[1]
         self.S = uf.UnionFind(self.size)
         self.adj_list =  None
+        self.k = 300   # number of nearest neighbors
+
+        ann_idx = get_neighbors(self.image, n_trees=10)
+
+        for i in range(self.size):
+            nbrs = ann_idx.get_nns_by_item(i, self.k, include_distances=True)
+            print(f"{nbrs=}")
 
 
 
